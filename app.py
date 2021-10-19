193285895ca1ff76b679102dde9f34645e987343
Alec <alec.arnold@lincolnuni.ac.nz>
2021-10-19 16:25:54 +1300

    Added the update member contacts page
    Allow Club Members to update their contacts.

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def getCursor():
         return dbconn
 #define the cursor action to get the data by connecting to database and enable the connection.
 def genID():
-    return uuid.uuid4().fields[1]
+    return uuid.uuid4().fields[1] #Take out the second number of the six numbers make up of uuid.
 
 
 @app.route("/", methods=['GET','POST']) #Default route - home page.
@@ -60,6 +60,30 @@ def getMembers():
     print(f"{Fixtures}")
     return render_template('members.html',Members=member,clubmember=Members,ClubNews=select_News,News=ClubNews,Fixtureresult=select_Fixture,fixturecolumn=Fixtures)
 
+@app.route('/member/update', methods=['GET','POST']) #Member page
+def Updatemembercontacts():
+    if request.method == 'POST':
+            MemberFirstName = request.form.get('MemberFirstName')
+            MemberLastName = request.form.get('MemberLastName')
+            Address1 = request.form.get('Address1')
+            Address2 = request.form.get('Address2')
+            City = request.form.get('City')
+            Email  = request.form.get('Email')
+            Phone = request.form.get('Phone')
+            cur = getCursor()
+            cur.execute("UPDATE Members SET MemberFirstName=%s, MemberLastName=%s, Address1=%s, Address2=%s, City=%s, Email=%s, Phone=%s where MemberID=%s",(MemberFirstName,MemberLastName,Address1,Address2,City,Email,str(Phone),))
+            return redirect("/")
+    else:
+        MemberID = request.args.get('MemberID')
+        if MemberID == '':
+            return redirect("/") #check whether they have a memberID.
+        else:
+            cur = getCursor()
+            cur.execute("SELECT * FROM Members where MemberID=%s",(str(MemberID),))
+            select_MemberID = cur.fetchone()
+            print(select_MemberID)
+            return render_template('updatecontacts.html',Memberdetails = select_MemberID)
+
 @app.route('/admin', methods=['GET','POST']) #Admin page
 def getAdmin():
     MemberID = request.args.get("MemberID")
@@ -70,44 +94,5 @@ def getAdmin():
     admin = [desc[0] for desc in cur.description]
     print(f"{admin}")
     return render_template('admin.html',admin=select_admin)
-
-@app.route('/members/update', methods=['GET','POST']) #Member page
-def MembersUpdate():
-    if request.method == 'POST':
-            Email  = request.form.get('Email')
-            Phone = request.form.get('Phone')
-            Address1 = request.form.get('Address1')
-            Address2 = request.form.get('Address2')
-            cur = getCursor()
-            cur.execute("UPDATE Members SET Email=%s, Address1=%s, Address2=%s, Phone=%s, where id=%s",(Email,Address1,Address2,str(Phone)))
-            return redirect("/")
-    else:
-        MemberID = request.args.get('MemberID')
-        if MemberID == '':
-            return redirect("/")
-        else:
-            cur = getCursor()
-            cur.execute("SELECT * FROM Members where MemberID=%s",(str(MemberID),))
-            select_MemberID = cur.fetchone()
-            print(select_MemberID)
-            return render_template('members.html',Members = select_MemberID)
-
-#@app.route('/customer', methods=['GET','POST'])
-#def customer():
-    #if request.method == 'POST':
-        #print(request.form)
-        #id = genID()
-        #print(id)
-        #company = request.form.get('companyname')
-        #contact = request.form.get('contactname')
-        #cur = getCursor()
-        #cur.execute("INSERT INTO customers(customer_id, company_name, contact_name) VALUES (%s,%s,%s);",(str(id),company,contact,))
-        #cur.execute("SELECT * FROM customers where customer_id=%s",(str(id),))
-        #select_result = cur.fetchall()
-        #column_names = [desc[0] for desc in cur.description]
-        #return render_template('dbresult.html',dbresult=select_result,dbcols=column_names)
-    #else:
-        #return render_template('customerform.html')
-
 
 
