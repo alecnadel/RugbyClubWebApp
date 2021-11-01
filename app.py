b6255c3fde8153b000c986cffa006c10b82dfe37
Alec <alec.arnold@lincolnuni.ac.nz>
2021-11-01 23:16:55 +1300

    Check and edit the functions
    To fix some errors use line by line debug tool from VS code to check the error and trying the fix it in add new fixtures and add new team.

--- app.py
+++ app.py
@@ -29,8 +29,10 @@ def getCursor():
             dbconn=None
             return getCursor()
 #Define the cursor function to get the data by connecting to database and enable the connection.
+
 def genID():
-    return uuid.uuid4().fields[1] #Take out the second number of the six numbers make up of uuid.
+    return uuid.uuid4().fields[1] 
+#Take out the second number of the six numbers make up of uuid.
 
 
 @app.route("/", methods=['GET','POST']) #Default route - home page.
@@ -100,6 +102,9 @@ def Updatemembercontacts():
             return render_template('updatecontacts.html',Memberdetails=select_MemberID)
 #Last bit of code getting information from the members table and get all the data from html page and set Jinja variable name when use for loop to loop over the data correctly.
 
+#Admin page function with Club news and new rugby team display on the webpage.
+#Use MemberID to identify the admin access and display the News and Teams updates.
+#Two MySQL queries here to select the data we want to see on the admin page.
 @app.route('/admin', methods=['GET','POST']) #Admin page
 def getAdmin():
     MemberID = request.args.get('MemberID')
@@ -111,10 +116,16 @@ def getAdmin():
     cur.execute("select ClubID, TeamName, TeamGrade from Teams;")
     all_teams = cur.fetchall()
     tm = [desc[0] for desc in cur.description]
-    return render_template('admin.html',team=all_teams,selectteam=tm,ClubNews=select_News,News=ClubNews)
-#Use MemberID identify the admin access and display the News and Teams updates.
-#Two MySQL queries here to select the data we want to see on the admin page.
+    cur.execute("""select FixtureID, FixtureDate, HT.TeamName as HomeTeamName, AWT.TeamName as AwayTeamName from Fixtures join Teams as HT on Fixtures.HomeTeam = HT.TeamID
+join Teams as AWT on Fixtures.AwayTeam = AWT.TeamID;""")
+    new_game = cur.fetchall()
+    nfixture = [desc[0] for desc in cur.description]
+    return render_template('admin.html',team=all_teams,selectteam=tm,ClubNews=select_News,News=ClubNews,fix=new_game,new=nfixture)
 
+#This route function allow admin to add new clubnews to the club, request is the word when upload the new data when user submit the form.
+#Two MySQL queries here to get the right data and add it to the database with different set of variable names at the end for calling.
+#ClubID is default to 23 because that is only club admin belong to the club is able to add news to the club.
+#In here use redirect to link the URL given to bring user back to the admin page.
 @app.route('/admin/updatenews', methods=['GET', 'POST'])
 def addnews():
     if request.method == 'POST':
@@ -131,11 +142,9 @@ def addnews():
         return redirect(url_for('getAdmin',pickmember=MemberID,dbnew=newscol))
     else:
         return render_template('addnews.html')
-#This route function allow admin to add new clubnews to the club, request is the word when upload the new data when user submit the form.
-#Two MySQL queries here to get the right data and add it to the database with different set variable names at the end for calling.
-#ClubID is default to 23 because that is only club admin belong to the club is able to add news to the club.
-#In here use redirect to link the URL given to bring user to the right place they want to go on the page.
 
+#For admin to see all the members, whether they are active or not. 'GET' here is to read the information, but not submitting or editing.
+#Print a report of all active members can be done through the next function from the admin page.
 @app.route('/admin/clubmember', methods=['GET', 'POST'])
 def viewmember():
     if request.method == 'GET':
@@ -145,9 +154,10 @@ def viewmember():
         coldbresult = [desc[0] for desc in cur.description]
         print(f"{coldbresult}")
         return render_template('viewmember.html',dbmemberresult=memberresult,col=coldbresult)
-#For admin to see all the members, whether they are active or not. GET here is to read the information, but not submitting or editing.
-#Print a report of all active members can be done through the next function within the admin page.
 
+#This page show all the active member where the previous function display all the club members whether they are active or inactive.
+#This page also suitable for printing with less member details which the admin do not need to know the rest of the member details
+#which can be found in the previous function via 'viewclubmember' page.
 @app.route('/admin/activemembers', methods=['GET', 'POST'])
 def viewactivemember():
     if request.method == 'GET':
@@ -157,10 +167,11 @@ def viewactivemember():
         mber = [desc[0] for desc in cur.description]
         print(f"{mber}")
         return render_template('activemembers.html',dbactive=activember,colmber=mber)
-#This page show all the active member where the previous function display all the club members whether they are active or inactive.
-#This page also suitable for printing with less member details which the admin do not need to know. And the rest of the member details
-#can be found in the previous function view club member page.
 
+#Add member allow admin to add a new member with 'POST' method when submitting the form to let the system know to update the database.
+#Insert and select queries here is to ensure all the variables are in orders for the display on the page correctly, %s is a placeholder for new data insert to database.
+#else condition is the 'GET' request for a drop down menu show list of TeamIDs available for members to join a team.
+#Set membership status=1, mean adding a active member and default their admin access for new member to 0, mean no admin access for new member.
 @app.route('/admin/clubmember/add', methods=['GET','POST'])
 def addmember():
     if request.method == 'POST':
@@ -182,19 +193,21 @@ def addmember():
         Email, Phone, Birthdate, MembershipStatus, AdminAccess) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);""",
         (str(id),First_Name,Last_Name,Address1,Address2,City,Email,Phone,Birthdate,Memberstatus,Adminaccess,))
         cur.execute("""SELECT MemberID, MemberFirstName, MemberLastName, Address1, Address2, City, Email, Phone, 
-        Birthdate, MembershipStatus, AdminAccess FROM Members where MemberID=%s;""",(str(id),))
+        Birthdate, MembershipStatus, AdminAccess FROM Members where MemberID=%s and MembershipStatus=1 and AdminAccess=0;""",(str(id),))
         select_member = cur.fetchall()
         membercol = [desc[0] for desc in cur.description]
-        return render_template('viewmember.html',insertmember=select_member,mber=membercol)
+        return redirect(url_for('viewmember',insertmember=select_member,mber=membercol))
     else:
         cur = getCursor()
         cur.execute("select TeamID, TeamName from Teams;")
         selectid = cur.fetchall()
         return render_template('addmember.html',team=selectid)
-#Add member allow admin to add a new member with POST method when submit the form to let the system know to update the database.
-#Insert and select queries here ensure all the variables are in orders for the display on the page correctly, %s is placeholder for new data insert to database.
-#else condition is the GET request for a drop down menu show list of TeamID available for member to join a team.
 
+#This function allow admin to update and edit the existing member details use 'POST' method to allow admin submit the form to the system for changes made.
+#Update is the keyword let the computer know update the row of data to the system and display them with the same order of the variable names after the query.
+#Updatemember also allow admin to assign a member to a team same as addmember function can assign a team there as well.
+#Check the Member with valid ID, if not, bring you back to the home page.
+#Get all the member details from Members table in order for admin to update the Members table data.
 @app.route('/admin/clubmember/update', methods=['GET', 'POST'])
 def updatemember():
     if request.method == 'POST':
@@ -227,20 +240,19 @@ def updatemember():
             memberresult = cur.fetchone()
             print(memberresult)
             return render_template('updatemember.html',memberdetails=memberresult)
-#This function allow admin to update edit the existing member details use POST method allow submit the form to the system for changes made.
-#Update is the keyword let the computer know update the row of data to the system and display them with the same order of the variable names after the query.
-#Check the Member with valid ID, if not, bring you back to the home page.
-#Get all the member details from Members table in order for admin to update the Members table data.
 
+#This part allow admin to add new rugby team and a new opposition team for them to play the game.
+#TeamID will be generated when a new team added to the system, same with the opposition team.
+#Here use insert and select to add the new team data to the Teams table and get all the information from Teams table in order to do that.
 @app.route('/admin/newteam', methods=['GET', 'POST'])
 def addnewteam():
     if request.method == 'POST':
         print(request.form)
         clubid = request.form.get('ClubID')
         teamname = request.form.get('TeamName')
-        teamgrade = request.form.get('teamgrade')
+        teamgrade = request.form.get('TeamGrade')
         cur = getCursor()
-        cur.execute("insert into Teams(ClubID, TeamName, TeamGrade) values (%s,%s,%s);",(str(clubid),teamname,str(teamgrade),))
+        cur.execute("insert into Teams(ClubID, TeamName, TeamGrade) values (%s,%s,%s);",(clubid,teamname,teamgrade,))
         cur.execute("select * from Teams;")
         select_team = cur.fetchall()
         teamscol = [desc[0] for desc in cur.description]
@@ -250,10 +262,11 @@ def addnewteam():
         cur.execute("select ClubID, TeamGrade from Teams;")
         selectgrade = cur.fetchall()
         return render_template('newteam.html',choosegrade=selectgrade)
-#This part allow admin to add new rugby team and a new opposition team for them to play the game.
-#TeamID will be generated when a new team added to the system, same with the opposition team.
-#Here use insert and select to add the new team data to the Teams table and get all the information from Teams table in order to do that.
 
+#Below function is add a new game for two different rugby teams to play and the two different teams will play in the same grade.
+#Here again use insert SQL query to add it onto database and select to get the data from SQL database.
+#Two selects use join table from SQL query to get the teamnames with hometeam and awayteam to show two different teams play on the same grade.
+#Redirect route used to get the admin back to their club admin page when a new fixture is add to the club.
 @app.route('/admin/rugbygame', methods=['GET', 'POST'])
 def addnewgame():
     if request.method == 'POST':
@@ -263,13 +276,23 @@ def addnewgame():
         awayteam = request.form.get('AwayTeam')
         cur = getCursor()
         cur.execute("insert into Fixtures(FixtureDate, HomeTeam, AwayTeam) value (%s,%s,%s);",(fixturedate,hometeam,awayteam,))
-        cur.execute("select * from Fixtures;") #Join table select teams join grade tables. TeamGrade and GradeID are dataset to join.
+        cur.execute("""select FixtureID, FixtureDate, HomeTeam, AwayTeam, HT.TeamName as HomeTeamName, AWT.TeamName as AwayTeamName from Fixtures 
+                    join Teams as HT on Fixtures.HomeTeam = HT.TeamID
+                    join Teams as AWT on Fixtures.AwayTeam = AWT.TeamID;""")
         ht = cur.fetchall()
         awyteam = [desc[0] for desc in cur.description]
-        return render_template('admin.html',hoeteam=ht,awayt=awyteam)
+        return redirect(url_for('getAdmin',hoeteam=ht,awayt=awyteam))
     else:
-        return render_template('newfixtures.html')
+        cur = getCursor()
+        cur.execute("""select FixtureID, FixtureDate, HomeTeam, AwayTeam, HT.TeamName as HomeTeamName, AWT.TeamName as AwayTeamName from Fixtures 
+                    join Teams as HT on Fixtures.HomeTeam = HT.TeamID
+                    join Teams as AWT on Fixtures.AwayTeam = AWT.TeamID;""")
+        select_ht = cur.fetchall()
+        awaytm = [desc[0] for desc in cur.description]
+        return render_template('newfixtures.html',hoteam=select_ht,tmaway=awaytm)
 
+#Below code is show suitable format printing all active members from the club who is eligible to play in a rugby team based on the min and max age from the grade table.
+#It will need two select statements and calculate the members team grade to determined they are able to play in the game based on the date entered.
 @app.route('/admin/eligibility', methods=['GET', 'POST'])
 def eligibility():
     if request.method == 'GET':
